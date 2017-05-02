739506068755da3b37a9d128788aa35d35e9b01c
XiaoChen <1468059916@qq.com>
2017-05-02 14:12:22 +0800

    实现从后台读取组织数据，并进行前端展示

--- AngularJS/test-tree3/app.js
+++ AngularJS/test-tree3/app.js
@@ -4,9 +4,9 @@
 
 var app = angular.module('treeDemo',['treeGrid']);
 
-app.controller('mainCtrl', function ($scope) {
+app.controller('mainCtrl', function ($scope, $http) {
 
-    $scope.tree_data = [
+    $scope.tree_dataori = [
         {Name:"USA",Area:9826675,Population:318212000,TimeZone:"UTC -5 to -10",
             children:[
                 {Name:"California", Area:423970,Population:38340000,TimeZone:"Pacific Time",
@@ -25,96 +25,96 @@ app.controller('mainCtrl', function ($scope) {
         {Name:"Texas",Area:268581,Population:26448193,TimeZone:"Mountain",children:[]}
     ];
 
-    // $scope.tree_data=[
-    //     {
-    //     "id": "90b5e399-1419-11e7-8bf3-5863ea6b5879",
-    //     "name": "航天长峰",
-    //     "children": [
-    //         {
-    //             "id": "1",
-    //             "name": "研发部",
-    //             "children": [
-    //                 {
-    //                     "id": "50971fae-141a-11e7-8bf3-5863ea6b5879",
-    //                     "name": "指挥云组",
-    //                     "children": [
-    //                         {
-    //                             "id": "a07a2957-0812-11e7-befe-7c6896b5c9d2",
-    //                             "nodeName": "Lion士大夫asdf"
-    //                         },
-    //                         {
-    //                             "id": "b3d360ba-0812-11e7-befe-7c6896b5c9d2",
-    //                             "nodeName": "Adazhangboa"
-    //                         }
-    //                     ]
-    //                 },
-    //                 {
-    //                     "id": "576c5e58-141a-11e7-8bf3-5863ea6b5879",
-    //                     "nodeName": "视频大数据",
-    //                     "children": [
-    //                         {
-    //                             "id": "c5449c88-0812-11e7-befe-7c6896b5c9d2",
-    //                             "nodeName": "Sherry"
-    //                         },
-    //                         {
-    //                             "id": "643895b1-09e2-11e7-9114-c51f4c21be57",
-    //                             "nodeName": "adminUser"
-    //                         }
-    //                     ]
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             "id": "49afd075-141a-11e7-8bf3-5863ea6b5879",
-    //             "nodeName": "人事部",
-    //             "children": [
-    //                 {
-    //                     "id": "54062387-141a-11e7-8bf3-5863ea6b5879",
-    //                     "nodeName": "人事组"
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             "id": "4d850cb7-141a-11e7-8bf3-5863ea6b5879",
-    //             "nodeName": "市场部"
-    //         }
-    //     ]
-    // }];
+    $scope.tree_data=[
+        {
+        "id": "90b5e399-1419-11e7-8bf3-5863ea6b5879",
+        "Name": "航天长峰",
+        "children": [
+            {
+                "id": "1",
+                "Name": "研发部",
+                "children": [
+                    {
+                        "id": "50971fae-141a-11e7-8bf3-5863ea6b5879",
+                        "Name": "指挥云组",
+                        "children": [
+                            {
+                                "id": "a07a2957-0812-11e7-befe-7c6896b5c9d2",
+                                "Name": "Lion士大夫Name"
+                            },
+                            {
+                                "id": "b3d360ba-0812-11e7-befe-7c6896b5c9d2",
+                                "Name": "Adazhangboa"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "576c5e58-141a-11e7-8bf3-5863ea6b5879",
+                        "Name": "视频大数据",
+                        "children": [
+                            {
+                                "id": "c5449c88-0812-11e7-befe-7c6896b5c9d2",
+                                "Name": "Sherry"
+                            },
+                            {
+                                "id": "643895b1-09e2-11e7-9114-c51f4c21be57",
+                                "Name": "adminUser"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "id": "49afd075-141a-11e7-8bf3-5863ea6b5879",
+                "Name": "人事部",
+                "children": [
+                    {
+                        "id": "54062387-141a-11e7-8bf3-5863ea6b5879",
+                        "Name": "人事组"
+                    }
+                ]
+            },
+            {
+                "id": "4d850cb7-141a-11e7-8bf3-5863ea6b5879",
+                "Name": "市场部"
+            }
+        ]
+    }];
 
     $scope.tree_data1 =
         [{
             "id": "90b5e399-1419-11e7-8bf3-5863ea6b5879",
-            "name": "航天长峰",
+            "Name": "航天长峰",
             "children": [
                 {
                     "id": "1",
-                    "name": "研发部",
+                    "Name": "研发部",
                     "children": [
                         {
                             "id": "50971fae-141a-11e7-8bf3-5863ea6b5879",
-                            "name": "指挥云组",
+                            "Name": "指挥云组",
                             "children": [
                                 {
                                     "id": "a07a2957-0812-11e7-befe-7c6896b5c9d2",
-                                    "name": "Lion士大夫asdf"
+                                    "Name": "Lion士大夫Name"
                                 },
                                 {
                                     "id": "b3d360ba-0812-11e7-befe-7c6896b5c9d2",
-                                    "name": "Adazhangboa"
+                                    "Name": "Adazhangboa"
                                 }
                             ]
                         },
                         {
                             "id": "576c5e58-141a-11e7-8bf3-5863ea6b5879",
-                            "name": "视频大数据",
+                            "Name": "视频大数据",
                             "children": [
                                 {
                                     "id": "c5449c88-0812-11e7-befe-7c6896b5c9d2",
-                                    "name": "Sherry"
+                                    "Name": "Sherry"
                                 },
                                 {
                                     "id": "643895b1-09e2-11e7-9114-c51f4c21be57",
-                                    "name": "adminUser"
+                                    "Name": "adminUser"
                                 }
                             ]
                         }
@@ -122,91 +122,38 @@ app.controller('mainCtrl', function ($scope) {
                 },
                 {
                     "id": "49afd075-141a-11e7-8bf3-5863ea6b5879",
-                    "name": "人事部",
+                    "Name": "人事部",
                     "children": [
                         {
                             "id": "54062387-141a-11e7-8bf3-5863ea6b5879",
-                            "name": "人事组"
+                            "Name": "人事组"
                         }
                     ]
                 },
                 {
                     "id": "4d850cb7-141a-11e7-8bf3-5863ea6b5879",
-                    "name": "市场部"
+                    "Name": "市场部"
                 }
             ]
         }];
 
-    $scope.tree_data2 =
-        [
-            {
-                "id": "90b5e399-1419-11e7-8bf3-5863ea6b5879",
-                "nodeName": "航天长峰",
-                "children": [
-                    {
-                        "id": "1",
-                        "nodeName": "研发部",
-                        "children": [
-                            {
-                                "id": "50971fae-141a-11e7-8bf3-5863ea6b5879",
-                                "nodeName": "指挥云组",
-                                "children": [
-                                    {
-                                        "id": "a07a2957-0812-11e7-befe-7c6896b5c9d2",
-                                        "nodeName": "Lion士大夫asdf",
-                                        "children": []
-                                    },
-                                    {
-                                        "id": "b3d360ba-0812-11e7-befe-7c6896b5c9d2",
-                                        "nodeName": "Adazhangboa",
-                                        "children": []
-                                    }
-                                ]
-                            },
-                            {
-                                "id": "576c5e58-141a-11e7-8bf3-5863ea6b5879",
-                                "nodeName": "视频大数据",
-                                "children": [
-                                    {
-                                        "id": "c5449c88-0812-11e7-befe-7c6896b5c9d2",
-                                        "nodeName": "Sherry",
-                                        "children": []
-                                    },
-                                    {
-                                        "id": "643895b1-09e2-11e7-9114-c51f4c21be57",
-                                        "nodeName": "adminUser",
-                                        "children": []
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        "id": "49afd075-141a-11e7-8bf3-5863ea6b5879",
-                        "nodeName": "人事部",
-                        "children": [
-                            {
-                                "id": "54062387-141a-11e7-8bf3-5863ea6b5879",
-                                "nodeName": "人事组",
-                                "children": []
-                            }
-                        ]
-                    },
-                    {
-                        "id": "4d850cb7-141a-11e7-8bf3-5863ea6b5879",
-                        "nodeName": "市场部",
-                        "children": []
-                    }
-                ]
-            }
-        ];
+    $scope.ha = {};
+    $http({
+        url:"http://192.168.5.183:9090/ma/department/90b5e399-1419-11e7-8bf3-5863ea6b5879/tree",
+        method:'GET'
+    }).success(function (data) {
+        $scope.ha = data.data;
+        alert("success");
+    });
 
 
-    // $scope.expanding_property={
-    //     filed:"nodeName",
-    //     displayName:'haha',
-    //     cellTemplate:"<i>{{row.branch[expandingProperty.filed]}}"
-    // };
+
+    // cellTeaplate : 每一行显示的是哪一个字段
+    $scope.expanding_property={
+        filed:"name",
+        displayName:'组织架构',
+        cellTemplate:"<i>{{row.branch[expandingProperty.filed]}}"
+    };
 
 
     $scope.col_defs = [
